bb680858b2477ed30f3f10b14c42b64e8d5c0b23
Chuyeon Lee <wonup2@hotmail.com>
2022-08-31 21:01:34 -0700

    Update CSES1068WeirdAlgorithm.java

--- 00_General/src/CSES1068WeirdAlgorithm.java
+++ 00_General/src/CSES1068WeirdAlgorithm.java
@@ -1,37 +1,28 @@
 import java.util.*;
-import java.io.*;
+
 
 public class CSES1068WeirdAlgorithm {
 
-	static BufferedReader in;
-	//static StringTokenizer st;
-	static long n;
-	static StringBuilder sb;
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		// TODO Auto-generated method stub
-			
-		in = new BufferedReader(new InputStreamReader(System.in));
-		
-		init();
-		solve();
-
-	}
-
-	static void init() throws NumberFormatException, IOException {
-		
-		n = Long.parseLong(in.readLine());
-		sb = new StringBuilder();
-		sb.append(n);
-	}
+	static Scanner in;
 	
-	static void solve() {
+	static long n;
+	
+	public static void main(String[] args) {
+
+			
+		in = new Scanner(System.in);
+		
+		n = in.nextLong();
+
+		System.out.print(n);
 		
 		while(n!=1) {
-			if(n%2==0) n/=2;
-			else n=n*3+1;
-			sb.append(" ").append(n);
+			
+			if(n%2==0) n = n/2;
+			else n = n*3 +1;
+			
+			System.out.print(" "+n);
 		}
-		
-		System.out.println(sb.toString());
 	}
+	
 }
