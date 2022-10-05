dcc614415ea814394eca89ff2d00aec76eba16fd
Chuyeon Lee <wonup2@hotmail.com>
2022-10-04 18:37:26 -0700

    .

--- 01_Bronze_CompleteSearch/src/CSES1623AppleDivision.java
+++ 01_Bronze_CompleteSearch/src/CSES1623AppleDivision.java
@@ -15,7 +15,7 @@ public class CSES1623AppleDivision {
 	}
 	
 	static void init() {
-		n=in.nextInt();
+		n = in.nextInt();
 		a = new long[n];
 		for(int i=0; i<n; i++)
 			a[i] = in.nextLong();
@@ -28,22 +28,21 @@ public class CSES1623AppleDivision {
 		
 		int num = (int)Math.pow(2, n);
 		String bin = "";
-		int diff = 0;
+
 		for(int i=0; i<num; i++) {
+			
 			bin = Integer.toBinaryString(i);
 			b = new boolean[n];
-			diff = n-bin.length();
 			
-			for(int j=0; j<bin.length(); j++) {
-				char c=bin.charAt(j);
-				if(c=='1') b[n-j-1]=true;
-			}
+			for(int j=0; j<bin.length(); j++) 
+				b[n-j-1]= bin.charAt(j)=='1'? true:false;			
 			
 			long x = 0l, y=0l;
 			for(int j=0; j<b.length;j++) {
 				if(b[j]) x+=a[j];
 				else y+=a[j];
 			}
+			
 			min = Math.min(min, Math.abs(x-y));
 		}
 		
